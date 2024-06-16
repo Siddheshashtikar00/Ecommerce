6a9417c3c022e101b8449388b3841e910de78450
Siddhesh Ashtikar <siddheshashtikar00@outlook.com>
2024-06-16 12:44:23 +0530

    ecommerce

---
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import MainCarousel from '../../components/HomeCarosel/MainCarosel'
+
+const HomePage = () => {
+  return (
+
+
+    <div>
+            <MainCarousel />
+
+            <div>
+               
+
+            </div>
+
+
+    </div>
+  )
+}
+
+export default HomePage
