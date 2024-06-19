37c68e2df52b00ae73b02b278b7e7dc31212cc89
Siddhesh Ashtikar <siddheshashtikar00@outlook.com>
2024-06-19 23:14:23 +0530

    19-06

---
+++ src/customer/components/HomeSectionCarosel/HomeSectionCarosel.jsx
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+import AliceCarousel from "react-alice-carousel";
+import HomeSectionCard from "../HomeSectionCard/HomeSectionCard";
+import { Button } from "@mui/material";
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
+
+const HomeSectionCarosel = ({data,sectionName}) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const responsive = {
+    0: { items: 2 },
+    720: { items: 3 },
+    1024: { items: 5.5 },
+  };
+
+  const slidePrev = () => setActiveIndex(activeIndex - 1);
+  const slideNext = () => setActiveIndex(activeIndex + 1);
+
+  const syncActiveIndex = ({ item }) => setActiveIndex(item);
+
+  const items = data.slice(0, 10).map((item) => <HomeSectionCard product={item} />);
+
+  return (
+    <div className="border">
+      <h2 className="text-2xl font-extrabold text-gray-800 py-2">{sectionName}</h2>
+      <div className="relative p-5">
+        <AliceCarousel
+          items={items}
+          disableButtonsControls
+          responsive={responsive}
+          disableDotsControls
+          onSlideChanged={syncActiveIndex}
+          activeIndex={activeIndex}
+        />
+
+        {/* Right Button */}
+        {activeIndex !== items.length - 5 && (
+          <Button
+            variant="contained"
+            className="z-50"
+            onClick={slideNext}
+            sx={{
+              position: "absolute",
+              top: "45%",
+              right: "-2rem",
+              transform: "translateY(-50%) rotate(90deg)",
+              bgcolor: "white",
+            }}
+            color="primary"
+            aria-label="next"
+          >
+            <KeyboardArrowLeftIcon
+              sx={{ transform: "rotate(90deg)", color: "black" }}
+            />
+          </Button>
+        )}
+
+        {/* Left Button */}
+        {activeIndex !== 0 && (
+          <Button
+            onClick={slidePrev}
+            variant="contained"
+            className="z-50"
+            sx={{
+              position: "absolute",
+              top: "45%",
+              left: "-2rem",
+              transform: "translateY(-50%) rotate(-90deg)",
+              bgcolor: "white",
+            }}
+            color="primary"
+            aria-label="next"
+          >
+            <KeyboardArrowLeftIcon
+              sx={{ transform: "rotate(90deg)", color: "black" }}
+            />
+          </Button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default HomeSectionCarosel;
--- src/customer/pages/HomePage/HomePage.jsx
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import MainCarousel from '../../components/HomeCarosel/MainCarosel'
+import HomeSectionCarosel from '../../components/HomeSectionCarosel/HomeSectionCarosel'
+import { mens_kurta } from '../../components/Data/mens_kurta'
 
 const HomePage = () => {
   return (
@@ -8,8 +10,12 @@ const HomePage = () => {
     <div>
             <MainCarousel />
 
-            <div>
-               
+            <div className='space-y-10 py-20 flex flex-col justify-center px-5 lg:px-10'>
+               <HomeSectionCarosel data={mens_kurta} sectionName={"Men's Kurta"}/>
+               <HomeSectionCarosel data={mens_kurta} sectionName={"Men's Shoes"} />
+               <HomeSectionCarosel data={mens_kurta} sectionName={"Men's Shirt"}/>
+               <HomeSectionCarosel data={mens_kurta} sectionName={"Women's Saree"}/>
+               <HomeSectionCarosel data={mens_kurta} sectionName={"women's Dress"}/>
 
             </div>
 
